ecb9e8dad6a0807ec0f531c89bc0868dc8f145ce
Clay Loveless <clay@loveless.net>
2026-08-07 00:49:09 +0000

    bito 2.0.0

--- Formula/bito.rb
+++ Formula/bito.rb
@@ -4,26 +4,26 @@
 class Bito < Formula
   desc "Quality gate tooling for building-in-the-open artifacts"
   homepage "https://github.com/claylo/bito"
-  version "1.0.0"
+  version "2.0.0"
   license "Apache-2.0 OR MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "706362e68cd2279bfd772576c1cd7e4745a8b2567f6107e6f893664d295978c9"
+      url "https://github.com/claylo/bito/releases/download/v2.0.0/bito-#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 ""
     else
-      url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "19490d97efcd09167a21820eabe89d71cbbddc61b76de2263fb95e55867261f6"
+      url "https://github.com/claylo/bito/releases/download/v2.0.0/bito-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 ""
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a267875f0ce4dfa4c358610831fedf811e60d9aac83fc63ce51e41827dc8805f"
+      url "https://github.com/claylo/bito/releases/download/v2.0.0/bito-#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 ""
     else
-      url "https://github.com/claylo/bito/releases/download/v1.0.0/bito-#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8d22cb522185cd65841627e288181bd15de2613c7d7505742e50b61f33fff2f5"
+      url "https://github.com/claylo/bito/releases/download/v2.0.0/bito-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 ""
     end
   end
 
